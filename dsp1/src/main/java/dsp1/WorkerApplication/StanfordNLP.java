1e4f882b9beb6b1b44b3638f4f507c29a0ff24cb
mohamedtah22 <you@example.com>
2025-11-26 16:54:43 +0200

    updates

--- dsp1/src/main/java/dsp1/WorkerApplication/StanfordNLP.java
+++ dsp1/src/main/java/dsp1/WorkerApplication/StanfordNLP.java
@@ -1,76 +1,71 @@
 package dsp1.WorkerApplication;
 
-
+import dsp1.AWS;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.GrammaticalStructureFactory;
 
-import java.io.InputStream;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
 import java.io.File;
-import java.io.IOException;
-import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
 
 public class StanfordNLP {
+
+    private static final String BUCKET = "dsp-assignment1-2025111913";
 
     private MaxentTagger posTagger;
     private LexicalizedParser parser;
 
     public StanfordNLP() {
         try {
-            // Extract models from resources to /tmp
-            String posModelPath = extractResourceToTemp("/models/english-left3words-distsim.tagger");
-            String pcfgModelPath = extractResourceToTemp("/models/englishPCFG.ser.gz");
+            // 1) Download models from S3 to /tmp
+            String posModel = downloadModelFromS3("models/english-left3words-distsim.tagger");
+            String pcfgModel = downloadModelFromS3("models/englishPCFG.ser.gz");
 
-            // Load the Stanford models
-            posTagger = new MaxentTagger(posModelPath);
-            parser = LexicalizedParser.loadModel(pcfgModelPath);
+            // 2) Load them
+            posTagger = new MaxentTagger(posModel);
+            parser = LexicalizedParser.loadModel(pcfgModel);
 
         } catch (Exception e) {
             throw new RuntimeException("Failed to load Stanford models", e);
         }
     }
 
-    // Extract model file from JAR resources to a real file (needed by Stanford NLP)
-    private String extractResourceToTemp(String resourcePath) throws IOException {
-        InputStream in = getClass().getResourceAsStream(resourcePath);
+    // Download model file from S3 to local /tmp dir
+    private String downloadModelFromS3(String key) throws Exception {
 
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found inside JAR: " + resourcePath);
-        }
+        File local = new File("/tmp/" + key.replace("/", "_"));
+        local.getParentFile().mkdirs();
 
-        File temp = File.createTempFile("model", ".tmp");
-        temp.deleteOnExit();
+        AWS.getInstance().getS3().getObject(
+                GetObjectRequest.builder()
+                        .bucket(BUCKET)
+                        .key(key)
+                        .build(),
+                local.toPath()
+        );
 
-        try (FileOutputStream out = new FileOutputStream(temp)) {
-            in.transferTo(out);
-        }
-
-        return temp.getAbsolutePath();
+        return local.getAbsolutePath();
     }
 
-    // ---- POS tagging ----
     public String analyzePOS(String text) {
         return posTagger.tagString(text);
     }
 
-    // ---- Constituency Parsing (tree) ----
     public String analyzeConstituency(String text) {
         Tree tree = parser.parse(text);
         return tree.toString();
     }
 
-    // ---- Dependency Parsing ----
     public String analyzeDependency(String text) {
         Tree tree = parser.parse(text);
 
         GrammaticalStructureFactory gsf =
                 parser.treebankLanguagePack().grammaticalStructureFactory();
 
-        GrammaticalStructure gs =
-                gsf.newGrammaticalStructure(tree);
+        GrammaticalStructure gs = gsf.newGrammaticalStructure(tree);
 
         return gs.typedDependencies().toString();
     }
